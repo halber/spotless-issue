1766145d13af2964aa2b076393f267d209037392
Michael Halberstadt <michael.halberstadt@sap.com>
2024-09-25 15:14:31 +0200

    reproducer

---
+++ src/main/java/io/example/Example.java
@@ -0,0 +1,22 @@
+package io.example;
+
+// spotless:off
+/**
+ * some text
+ *
+ * <pre>
+ * {@code
+ * @SuppressWarnings("unused")
+ * public class SomeClass {
+ *
+ *     @Override
+ *     public String someMethod() {
+ *         return "something";
+ *     }
+ * }
+ * }</pre>
+ */
+// spotless:on
+public class Example {
+
+}
